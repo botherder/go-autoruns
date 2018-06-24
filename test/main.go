99c642f1ffddfb3e3d115ee8635dc180361617ae
Nex <nex@nex.sx>
2018-06-24 12:52:14 +0200

    Added test main

---
+++ test/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	autoruns ".."
+)
+
+func main() {
+	autoruns := autoruns.Autoruns()
+
+	for _, autorun := range(autoruns) {
+		fmt.Println(autorun.Type)
+		fmt.Println(autorun.Location)
+		fmt.Println(autorun.ImagePath)
+		fmt.Println(autorun.ImageName)
+		fmt.Println(autorun.Arguments)
+		fmt.Println("")
+	}
+}
