7087926d73f92b7efcfd38281c9dc626c5fd75b0
Nex <nex@nex.sx>
2018-09-02 23:17:41 +0200

    Adding support for FreeBSD rc.d

--- autoruns_freebsd.go
+++ autoruns_freebsd.go
@@ -1,5 +1,90 @@
 package autoruns
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var enabledServices *[]string
+
+func parseRCConf() {
+	file, err := os.Open("/etc/rc.conf")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	rxp, err := regexp.Compile("^(\\w+)_enabled=\"YES\"$")
+	if err != nil {
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := rxp.FindString(line)
+		if match == "" {
+			continue
+		}
+
+		enabledServices = append(enabledServices, match)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return
+	}
+}
+
+func parseRCScripts(entryType, folder string) (records []*Autorun) {
+	// Check if the folders exists.
+	if _, err := os.Stat(folder); os.IsNotExist(err) {
+		return
+	}
+
+	// Get list of files in folder.
+	filesList, err := ioutil.ReadDir(folder)
+	if err != nil {
+		return
+	}
+
+	rxp, err := regexp.Compile("^name=(\\w+)$")
+
+	// Loop through all files in folder.
+	for _, fileEntry := range filesList {
+		file, err := os.Open(fileEntry)
+		if err != nil {
+			continue
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			line = strings.TrimSpace(line)
+			line = strings.Replace("\"", "")
+			name := rxp.FindString(line)
+			if name == "" {
+				continue
+			}
+
+			newAutorun := Autorun{
+				Type:      entryType,
+				Location:  fileEntry,
+				ImageName: name,
+			}
+
+			records = append(records, &newAutorun)
+		}
+	}
+}
+
 func getAutoruns() (records []*Autorun) {
+	parseRCConf()
+
+	records = append(records, parseRCScripts("rc.d", "/etc/rc.d/")...)
+	records = append(records, parseRCScripts("local_rc.d", "/usr/local/etc/rc.d/")...)
 	return
 }
