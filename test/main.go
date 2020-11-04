d7013547b33d082fd4d523861d6be00e63edeb9f
nor3th-agent <>
2020-11-04 21:27:37 +0100

    Added Entry and LaunchString for better test display of the parsed information

--- test/main.go
+++ test/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	autoruns ".."
 	"fmt"
+	autoruns "github.com/botherder/go-autoruns"
 )
 
 func main() {
-	autoruns := autoruns.Autoruns()
+	autorunList := autoruns.Autoruns()
 
-	for _, autorun := range autoruns {
+	for _, autorun := range autorunList {
 		fmt.Println(autorun.Type)
 		fmt.Println(autorun.Location)
 		fmt.Println(autorun.ImagePath)
 		fmt.Println(autorun.ImageName)
 		fmt.Println(autorun.Arguments)
+		fmt.Println(autorun.Entry)
+		fmt.Println(autorun.LaunchString)
 		fmt.Println("")
 	}
 }
